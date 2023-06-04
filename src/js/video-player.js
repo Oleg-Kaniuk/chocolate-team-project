449b927344f7815757338a237b5b2b38cf838f2a
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-06-04 20:15:01 +0200

    add img 1x

--- src/js/video-player.js
+++ src/js/video-player.js
@@ -1,28 +1,28 @@
 // Інінціалізація програвача
 const player = new Plyr("#features-player");
 // Шлях до постера для програвача
-player.poster = "https://iili.io/HNysfaf.jpg";
-// player.poster = "/img/how_it’s_made/cover@1x.jpg";
-// const posters = [
-//   { src: "../img/how_it’s_made/cover-mobile@1x.jpg", mediaQuery: "(max-width: 374px)" }, // маленький розмір вьюпорту
-//   { src: "../img/how_it’s_made/cover-tablet@1x.jpg", mediaQuery: "(min-width: 375px) and (max-width: 767px)" }, // середній розмір вьюпорту
-//   { src: "../img/how_it’s_made/cover@1x.jpg", mediaQuery: "(min-width: 768px)" }, // великий розмір вьюпорту
-// ];
-// // Функція для зміни постера в залежності від розміру вьюпорту
-// function changePoster() {
-//   const currentPoster = player.poster;
+// player.poster = "https://iili.io/HNysfaf.jpg";
 
-//   // Перевіряємо кожен постер і встановлюємо потрібний постер
-//   for (const poster of posters) {
-//     if (window.matchMedia(poster.mediaQuery).matches) {
-//       if (currentPoster !== poster.src) {
-//         player.poster = poster.src;
-//       }
-//       break;
-//     }
-//   }
-// }
+const posters = [
+  { src: "../img/how_it’s_made/cover-mobile@1x.jpg", mediaQuery: "(max-width: 374px)" }, // маленький розмір вьюпорту
+  { src: "../img/how_it’s_made/cover-tablet@1x.jpg", mediaQuery: "(min-width: 375px) and (max-width: 767px)" }, // середній розмір вьюпорту
+  { src: "../img/how_it’s_made/cover@1x.jpg", mediaQuery: "(min-width: 768px)" }, // великий розмір вьюпорту
+];
+// Функція для зміни постера в залежності від розміру вьюпорту
+function changePoster() {
+  const currentPoster = player.poster;
 
-// // Викликаємо функцію після завантаження сторінки та при зміні розміру вьюпорту
-// window.addEventListener("DOMContentLoaded", changePoster);
-// window.addEventListener("resize", changePoster);
+  // Перевіряємо кожен постер і встановлюємо потрібний постер
+  for (const poster of posters) {
+    if (window.matchMedia(poster.mediaQuery).matches) {
+      if (currentPoster !== poster.src) {
+        player.poster = poster.src;
+      }
+      break;
+    }
+  }
+}
+
+// Викликаємо функцію після завантаження сторінки та при зміні розміру вьюпорту
+window.addEventListener("DOMContentLoaded", changePoster);
+window.addEventListener("resize", changePoster);
